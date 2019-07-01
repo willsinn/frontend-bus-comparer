07c198f4d7659e9bf8e3d6b8b96ab3805a045605
William Sinn <w.h.sinn@gmail.com>
2019-07-01 14:13:04 -0400

    change profile to class component

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,33 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Card, Image } from 'semantic-ui-react'
 import withAuth from '../hocs/withAuth'
 
 import Search from './Search'
 
-
-const Profile = ({ avatar, username, bio }) => (
-  <>
-    <Card>
-      <Image src={avatar} />
-      <Card.Content>
-        <Card.Header>{username}</Card.Header>
-        <Card.Description>{bio}</Card.Description>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Search />
-    </Card>
-  </>
-)
-
-// const mapStateToProps = (reduxStoreState) => {
-//   return {
-//     avatar: reduxStoreState.usersReducer.user.avatar,
-//     username: reduxStoreState.usersReducer.user.username,
-//     bio: reduxStoreState.usersReducer.user.bio
+class Profile extends Component {
+  render() {
+    return(
+      <>
+        <Card>
+          <Image src={this.props.avatar} />
+          <Card.Content>
+            <Card.Header>{this.props.username}</Card.Header>
+            <Card.Description>{this.props.bio}</Card.Description>
+          </Card.Content>
+        </Card>
+        <Search />
+      </>
+    )
+  }
+}
+//
+// const Profile = ({ avatar, username, bio }) => {
+//
+//   const renderProfile = () => {
+//
 //   }
+//   const renderSearches = () => {
+//     return (<Card>
+//     </Card>)
+//   }
+//
+//   const handleToggleClick = (e, renderSearches) => {
+//     renderSearches()
+//   }
+//   return (
+//
+//       <button onClick={(e) => {handleToggleClick(e, renderSearches)}}> HOLA </button>
+//       <Card>
+//         <Image src={avatar} />
+//         <Card.Content>
+//           <Card.Header>{username}</Card.Header>
+//           <Card.Description>{bio}</Card.Description>
+//         </Card.Content>
+//       </Card>
+//       <Search />
+//
+//       {renderSearches}
+//   )
 // }
+// // const mapStateToProps = (reduxStoreState) => {
+// //   return {
+// //     avatar: reduxStoreState.usersReducer.user.avatar,
+// //     username: reduxStoreState.usersReducer.user.username,
+// //     bio: reduxStoreState.usersReducer.user.bio
+// //   }
+// // }
 
 const mapStateToProps = ({ usersReducer: { user: { avatar, username, bio } } }) => ({
   avatar,
