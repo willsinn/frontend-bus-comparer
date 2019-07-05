745ca8152aa2a6ac9c6709ac952b30bbae5bd335
William Sinn <w.h.sinn@gmail.com>
2019-07-05 11:00:54 -0400

    add search fn with four params

--- src/components/ConsoleFilterFns.js
+++ src/components/ConsoleFilterFns.js
@@ -1,14 +1,38 @@
 import React from "react";
 
 const ConsoleFilterFns = props => {
-  console.log(props);
-  // const handleTimeClick = () => {};
-  const handlePriceClick = () => {};
+  console.log("consolefilter", props);
+  // Search cheapest prices with appropriate from-to-date-time
+  const handleFilterOptions = () => {
+    const route = props.searches.filter(
+      search =>
+        props.searchParams.start === search.start_from &&
+        props.searchParams.destination === search.to_destination
+    );
+
+    if (props.searchParams.date && route !== []) {
+      const filterDate = route.filter(
+        search => props.searchParams.date === search.date
+      );
+
+      if (props.searchParams.time && filterDate !== []) {
+        filterDate.map(date => {
+          const filterTime = date.items.filter(
+            item => props.searchParams.time === item.time
+          );
+          console.log(filterTime);
+          return filterTime;
+        });
+      }
+    }
+  };
+  const handleCurrentTimeClick = ({}) => {};
+
   return (
     <div>
       <button
         onClick={e => {
-          handlePriceClick();
+          handleFilterOptions(props.searches);
         }}
       >
         {" "}
@@ -19,6 +43,7 @@ const ConsoleFilterFns = props => {
 };
 
 export default ConsoleFilterFns;
+
 // <button onClick={handleTimeClick()}>
 //   {" "}
 //   Returns All Search-Items with specified time
--- src/components/SearchConsole.js
+++ src/components/SearchConsole.js
@@ -3,13 +3,22 @@ import ConsoleFilterFns from "./ConsoleFilterFns";
 
 class SearchConsole extends Component {
   state = {
-    console: []
+    console: [],
+    searchParams: {
+      date: "2019-07-12",
+      start: "NY, NY",
+      destination: "Maryland",
+      time: "03:00"
+    }
   };
   render() {
     console.log(this.props.searches);
     return (
       <div id="search-console">
-        <ConsoleFilterFns />
+        <ConsoleFilterFns
+          searches={this.props.searches}
+          searchParams={this.state.searchParams}
+        />
         <div className="render-search-console" />
       </div>
     );
