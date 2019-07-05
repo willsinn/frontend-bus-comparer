428a5a2e69a921a59c4f1a829da520fd9e77633b
William Sinn <w.h.sinn@gmail.com>
2019-07-05 17:32:33 -0400

    add route, rendering on app, implement hover & click to view

--- src/components/SearchConsole.js
+++ src/components/SearchConsole.js
@@ -13,19 +13,24 @@ class SearchConsole extends Component {
     }
   };
   handleSearchSubmit = searchParams => {
-    const params = searchParams;
     this.setState({ searchParams: searchParams });
-    // this.handleFilterOptions();
-    const route = this.filterRoute(params);
-    const date = this.filterDate(params);
-    const time = this.filterTime(params);
+    const route = this.filterRoute(searchParams);
+    const date = this.filterDate(searchParams);
+    const time = this.filterTime(searchParams);
 
     this.setState({ results: [...this.state.results.concat(time)] });
   };
 
   renderConsoleItems = () => {
+    const route =
+      this.state.searchParams.start + "→" + this.state.searchParams.destination;
     return this.state.results.map(result => (
-      <SearchConsoleItem key={result.id} result={result} />
+      <SearchConsoleItem
+        key={result.id}
+        result={result}
+        route={route}
+        handleDeleteItem={this.handleDeleteItem}
+      />
     ));
   };
   filterRoute = params => {
@@ -48,6 +53,11 @@ class SearchConsole extends Component {
       });
     });
     return array;
+  };
+  handleDeleteItem = target => {
+    this.setState({
+      results: [...this.state.results.filter(result => result !== target)]
+    });
   };
 
   render() {
--- src/components/SearchConsoleItem.js
+++ src/components/SearchConsoleItem.js
@@ -1,6 +1,29 @@
 import React from "react";
 
-const SearchConsoleItem = () => {
-  return <div className="console-result-item">Hi</div>;
+const SearchConsoleItem = props => {
+  return (
+    <div id="console-item">
+      <div className="console-result-item-wrapper">
+        <button>
+          {" "}
+          <span>⭒</span>{" "}
+        </button>
+        <div className="console-result-item">
+          <div>{props.route}</div>
+
+          <div>{props.result.time}</div>
+          <div>{props.result.price}</div>
+        </div>
+        <button
+          onClick={e => {
+            props.handleDeleteItem(props.result);
+          }}
+        >
+          {" "}
+          delete{" "}
+        </button>
+      </div>
+    </div>
+  );
 };
 export default SearchConsoleItem;
