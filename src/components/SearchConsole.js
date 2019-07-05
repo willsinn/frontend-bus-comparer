803e8be87015c036153ad53fc7bc25d782c36e50
William Sinn <w.h.sinn@gmail.com>
2019-07-05 16:26:57 -0400

    add filter fns, add form, add item, renders features. remove console fns.

--- src/components/SearchConsole.js
+++ src/components/SearchConsole.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import ConsoleFilterFns from "./ConsoleFilterFns";
+import SearchConsoleItem from "./SearchConsoleItem";
+import SearchConsoleForm from "./SearchConsoleForm";
 
 class SearchConsole extends Component {
   state = {
-    console: [],
+    results: [],
     searchParams: {
       date: "2019-07-12",
       start: "NY, NY",
@@ -11,15 +12,60 @@ class SearchConsole extends Component {
       time: "03:00"
     }
   };
+  handleSearchSubmit = searchParams => {
+    const params = searchParams;
+    this.setState({ searchParams: searchParams });
+    // this.handleFilterOptions();
+    const route = this.filterRoute(params);
+    const date = this.filterDate(params);
+    const time = this.filterTime(params);
+
+    this.setState({ results: [...this.state.results.concat(time)] });
+  };
+
+  renderConsoleItems = () => {
+    return this.state.results.map(result => (
+      <SearchConsoleItem key={result.id} result={result} />
+    ));
+  };
+  filterRoute = params => {
+    return this.props.searches.filter(
+      search =>
+        search.start === params.start_from &&
+        search.destination === params.to_destination
+    );
+  };
+  filterDate = params => {
+    return this.props.searches.filter(search => search.date === params.date);
+  };
+  filterTime = params => {
+    const array = [];
+    this.props.searches.forEach(search => {
+      search.items.forEach(item => {
+        if (item.time === params.time) {
+          array.push(item);
+        }
+      });
+    });
+    return array;
+  };
+
   render() {
-    console.log(this.props.searches);
+    console.log(this.state.results);
     return (
       <div id="search-console">
-        <ConsoleFilterFns
-          searches={this.props.searches}
-          searchParams={this.state.searchParams}
-        />
-        <div className="render-search-console" />
+        <SearchConsoleForm handleSearchSubmit={this.handleSearchSubmit} />
+        <div className="render-search-console">
+          <div className="items-header">
+            <div>{this.state.searchParams.date}</div>
+            <div>{this.state.searchParams.start}</div>
+            <div>{this.state.searchParams.start}</div>
+          </div>
+
+          <div className="console-content-wrapper">
+            {this.renderConsoleItems()}
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/SearchConsoleItem.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const SearchConsoleItem = () => {
+  return <div className="console-result-item">Hi</div>;
+};
+export default SearchConsoleItem;
