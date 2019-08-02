39a1cb4d68553a36d0271870dd0e011799350928
William Sinn <w.h.sinn@gmail.com>
2019-08-01 20:18:31 -0400

    deployable

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Fragment } from 'react'
-import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
+import React, { Fragment } from "react";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 
-import Profile from './components/Profile'
-import LoginForm from './components/LoginForm'
-import SignupForm from './components/SignupForm'
-import Nav from './components/Nav'
-import NotFound from './components/NotFound'
-import Search from './components/Search';
-import './App.css'
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import Nav from "./components/Nav";
+import NotFound from "./components/NotFound";
+import Profile from "./components/Profile";
+import Search from "./components/Search";
+
+import "./App.css";
 
 const App = props => {
-  console.log('%c APP Props: ', 'color: firebrick', props)
+  console.log("%c APP Props: ", "color: firebrick", props);
   return (
     <Fragment>
       <Nav />
@@ -22,11 +23,10 @@ const App = props => {
         <Route exact path="/login" component={LoginForm} />
         <Route exact path="/signup" component={SignupForm} />
         <Route component={NotFound} />
-
       </Switch>
     </Fragment>
-  )
-}
+  );
+};
 
-export default withRouter(App)
+export default withRouter(App);
 //withRouter is a Higher Order Component (HOC) that returns a COPY of App with React router props injected
