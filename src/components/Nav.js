ddc360bab4bb86b6ab0e7396897ed0ea17bd022c
William Sinn <w.h.sinn@gmail.com>
2020-07-29 22:33:42 -0400

    add app logo, resize and center on login

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,8 +15,30 @@ const Nav = ({ user, location: { pathname } }) => {
   return (
     <>
       {user === null ? (
-        <div className="navbar" style={{ backgroundColor: "#003e74" }}>
-          <h3 style={{ color: "white", marginRight: "7.5%" }}>BUS COMPARER</h3>
+        <div
+          className="navbar"
+          style={{
+            backgroundColor: "#003e74",
+            justifyContent: "center",
+            marginRight: "7.5%",
+          }}
+        >
+          <img
+            src={require("../images/app-logo.png")}
+            height="40px"
+            width="40px"
+            alt=""
+            className="landing-bg"
+            style={{
+              position: "relative",
+              height: "52px",
+              weight: "40px",
+              marginTop: "8px",
+            }}
+          />
+          <h3 style={{ color: "white", marginLeft: "12px", paddingTop: "4px" }}>
+            BUS COMPARER
+          </h3>
         </div>
       ) : (
         <div className="navbar">
